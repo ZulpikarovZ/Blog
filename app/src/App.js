a3aee6df35e3f81584dc7c9dd1db8285d9f7075a
Zaur <Zulpikarov.Z@gmail.com>
2023-12-16 18:04:05 +0300

    install packages and fonts

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+	font-size: 64px;
+`;
+
+export const App = () => {
+	return (
+		<Div className="App">
+			<i className="fa fa-camera-retro"></i>
+		</Div>
+	);
+};
