c2c27b364ddad8955c7b02d47bd996bb9c8fd815
Zaur <Zulpikarov.Z@gmail.com>
2023-12-20 12:25:49 +0300

    Реализовать основной компонент блога с роутингом

---
+++ app/src/blog.js
@@ -0,0 +1,34 @@
+import { Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Content = styled.div`
+	padding: 120px 0;
+`;
+
+const H2 = styled.h2`
+	text-align: center;
+`;
+
+const Header = () => <div>Header</div>;
+const Footer = () => <div>Footer</div>;
+
+export const Blog = () => {
+	return (
+		<>
+			<Header></Header>
+			<Content>
+				<H2>Контент главной страницы</H2>
+				<Routes>
+					<Route path="/" element={<div>Главная страница</div>} />
+					<Route path="/login" element={<div>Авторизация</div>} />
+					<Route path="/register" element={<div>Регистрация</div>} />
+					<Route path="/users" element={<div>Пользователи</div>} />
+					<Route path="/post" element={<div>Статья</div>} />
+					<Route path="/post/post:id" element={<div>Новая статья</div>} />
+					<Route path="*" element={<div>Ошибка</div>} />
+				</Routes>
+			</Content>
+			<Footer></Footer>
+		</>
+	);
+};
